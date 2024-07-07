3a0b6a7d4f796c10135e14f418681558fd90d52b
BisaUmutoni <bclaudette636@gmail.com>
2024-07-07 09:42:52 +0200

    temp conversion added

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,7 @@
-# temp_conversion_tool.py
-
-# Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+#CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
@@ -11,7 +10,7 @@ def convert_to_celsius(fahrenheit):
 
 # Function to convert Celsius to Fahrenheit
 def convert_to_fahrenheit(celsius):
-    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
 # Main function to handle user interaction
